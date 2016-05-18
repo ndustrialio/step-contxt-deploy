29dd3b53bb023e61dd29759312d12247df381980
John Crawford <jrcrawfo@ncsu.edu>
2016-05-18 00:29:25 -0400

    Fixed support for json

--- run.sh
+++ run.sh
@@ -10,13 +10,12 @@ if [ -z "$WERCKER_CONTXT_DEPLOY_SERVICE_DESCRIPTOR" ]; then
   fail "Please provide a Contxt Service Descriptor"
 fi
 
-json="
-{
+json="{
     \"tag\": \"$WERCKER_GIT_COMMIT\"
 }"
 
 # post the result to the slack webhook
-RESULT=$(curl -d "payload=$json" -H "Authorization: $WERCKER_CONTXT_DEPLOY_TOKEN" -s "http://coordinator.api.ndustrial.io/v1/services/$WERCKER_CONTXT_DEPLOY_SERVICE_DESCRIPTOR/deploy" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
+RESULT=$(curl -d "$json" -H "Authorization: $WERCKER_CONTXT_DEPLOY_TOKEN" -H "Content-Type: application/json" -s "http://coordinator.api.ndustrial.io/v1/services/$WERCKER_CONTXT_DEPLOY_SERVICE_DESCRIPTOR/deploy" --output "$WERCKER_STEP_TEMP"/result.txt -w "%{http_code}")
 cat "$WERCKER_STEP_TEMP/result.txt"
 
 if [ "$RESULT" = "403" ]; then
